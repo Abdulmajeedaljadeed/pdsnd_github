932496e3dadf87c5d39808b48ea900126320ec1a
Abdulmajeedaljadeed <aljadeed.majeed@gmail.com>
2023-01-25 23:18:57 +0300

    refactor: add new method name

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -82,6 +82,8 @@ def station_stats(df):
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
 
+def name():
+    print(" Abdulmajeed Aljadeed")
 
 def trip_duration_stats(df):
     """Displays statistics on the total and average trip duration."""
